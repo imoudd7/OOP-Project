09c73a3ce0c6de6ef5bd005526e4e54a71bcbbbc
Mahmoud Daher <moudd.daher@gmail.com>
2024-12-23 13:46:38 +0300

    Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace OOP_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(424, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1402, 855);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dogru",
+            "Dik Dortgen",
+            "Cember",
+            "Kure",
+            "Ucgen",
+            "Nokta",
+            "Dik Dortgen Prizma"});
+            this.comboBox1.Location = new System.Drawing.Point(29, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Dogru",
+            "Dik Dortgen",
+            "Cember",
+            "Kure",
+            "Ucgen",
+            "Nokta",
+            "Dik Dortgen Prizma"});
+            this.comboBox2.Location = new System.Drawing.Point(29, 214);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(155, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 611);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 680);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(395, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 739);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(395, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 795);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(395, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(47, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "OBJECT (1)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(91, 544);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 28);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Some Collisions";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkRed;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(141, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 38);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Result";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(40, 451);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(331, 22);
+            this.textBox8.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(42, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "OBJECT (2)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "GEOMETRIC OBJECTS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(19, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(388, 22);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "After selecting the two Objects, click here";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.DarkRed;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(226, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 38);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Save";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1838, 854);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,380 @@
+﻿
+/****************************************************************************
+** SAKARYA ÜNİVERSİTESİ
+** BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+** BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+** PROGRAMLAMAYA GİRİŞİ DERSİ
+**Youtube Link: https://www.youtube.com/watch?v=e4TP2revs3c
+** ÖDEV NUMARASI: NDP Project
+** ÖĞRENCİ ADI: MAHMOUD ALDAHER
+** ÖĞRENCİ NUMARASI: G221210588
+** DERS GRUBU:  2/B
+****************************************************************************/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+
+
+            Random random = new Random();
+
+
+
+
+
+
+            int xCember, yCember, x2Cember, y2Cember;
+            xCember = random.Next(200, 250);
+            yCember = random.Next(50, 100);
+            x2Cember = random.Next(120, 150);
+            y2Cember = random.Next(120, 150);
+
+
+
+
+            Pen pen = new Pen(Color.DarkSeaGreen, 5);
+           
+                e.Graphics.DrawEllipse(pen, xCember, yCember, x2Cember, y2Cember);
+
+
+
+          
+
+            
+
+            // ucgenin koordinatlar
+            int ucgenX1, ucgenX2, ucgenY1, ucgenY2, ucgenZ1, ucgenZ2;
+
+            ucgenX1 = random.Next(25, 45); ucgenX2 = random.Next(25, 45);
+           ucgenY1 = random.Next(100, 145); ucgenY2 = random.Next(100, 145);
+            ucgenZ1 = 160; ucgenZ2 = 80;
+            // Üçgenin köşeleri
+            Point point1 = new Point(ucgenX1, ucgenX2);
+            Point point2 = new Point(ucgenY1, ucgenY2);
+            Point point3 = new Point(ucgenZ1, ucgenZ2);
+
+            //Bu Ukoseleri iceren bir dizidir
+            Point[] trianglePoints = { point1, point2, point3 };
+
+            //Bu Cizginin Rengi
+            Color lineColor = Color.White;
+
+            //Bu Cizginin kalinligi icin
+            int lineWidth = 2;
+
+            //Bu Cizginin nesnesii olusturma icim
+            Pen trianglePen = new Pen(lineColor, lineWidth);
+
+            // Üçgeni çizme
+            e.Graphics.DrawPolygon(trianglePen, trianglePoints);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            int xdogru, ydogru, x2dogru, y2dogru;
+            xdogru = random.Next(50, 100);
+            ydogru = random.Next(300, 330);
+            x2dogru = random.Next(300, 310);
+            y2dogru = random.Next(300, 330);
+
+
+
+
+
+
+
+            //Bu Dogru icin
+            Pen pen1 = new Pen(Color.DarkOrchid, 3);
+            Point pointdx = new Point(xdogru, x2dogru);
+            Point pointdy = new Point(ydogru, y2dogru);
+            e.Graphics.DrawLine(pen1, pointdx, pointdy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Graphics nokta = e.Graphics;
+            
+            int noktax, noktay;
+            noktax = random.Next(300, 340);
+            noktay = random.Next(80, 100);
+
+            //Nokta
+            Pen pen2 = new Pen(Color.DimGray, 3);
+            nokta.DrawEllipse(pen2, noktax, noktay, 2, 2);
+
+
+
+
+
+
+
+
+            Graphics h = e.Graphics;
+
+
+            int dikx1, dikx2, diky1, diky2;
+            dikx1 = random.Next(140, 145);
+            dikx2 = random.Next(40, 85);
+            diky1 = random.Next(250, 260);
+            diky2 = random.Next(60, 85);
+
+
+
+            // Dikdortgen
+            Pen pen3 = new Pen(Color.Purple, 7);
+            h.DrawRectangle(pen3, dikx1, diky1, dikx2 + dikx2, diky2+diky2);
+
+
+
+
+
+
+
+
+
+
+
+
+            Graphics kure = e.Graphics;
+
+            int kurex, kurey, kurex2, kurey2;
+            kurex = random.Next(400, 450);
+            kurey = random.Next(400, 450);
+            kurex2 = random.Next(150, 200);
+            kurey2 = random.Next(150, 250);
+
+           
+                
+                
+ 
+            //KURE
+            SolidBrush brush = new SolidBrush(Color.DeepPink);
+            kure.FillEllipse(brush, kurex, kurey, kurex2 , kurey2);
+
+
+
+
+
+
+            int dikpx1, dikpx2, dikpy1, dikpy2;
+
+            dikpx1 = random.Next(450, 500);
+            dikpx2 = random.Next(50, 150);
+            dikpy1 = random.Next(200, 210);
+            dikpy2 = random.Next(200, 210);
+
+
+                             
+
+
+            Graphics dik = e.Graphics;
+
+            //DikdOrtgen Prizma
+            SolidBrush brushdik = new SolidBrush(Color.Red);
+            SolidBrush brushdik2 = new SolidBrush(Color.DarkRed);
+            dik.FillRectangle(brushdik, dikpx1, dikpy1, dikpx2, dikpy2);
+            dik.FillRectangle(brushdik, dikpx1 + 1, dikpy1 + 1, dikpx2 + 5, dikpy2 + 3);
+            dik.FillRectangle(brushdik, dikpx1 + 2, dikpy1 + 2, dikpx2 + 5, dikpy2 + 3);
+            dik.FillRectangle(brushdik2, dikpx1 + 5, dikpy1 + 5, dikpx2 +5, dikpy2+3);
+
+
+
+
+
+            // -----------------------------------------------CARPISMA KOONTROLLERI----------------------------------------//
+
+
+
+           
+
+            // Dogru ve dikdortgen cakisma 
+            if (xdogru == x2dogru && ydogru == y2dogru)
+            {
+                textBox3.Text = ("Doğru sadece bir noktadan oluşuyor ve dikdörtgenle çakışamaz.");
+            }
+            else if (xdogru == x2dogru)
+            {
+                if ((ydogru > diky1 && ydogru < diky2) || (y2dogru > diky1 && y2dogru < diky2))
+                {
+                    textBox3.Text = ("Doğru dikdörtgenle çakışıyor.");
+                }
+                else
+                {
+                    textBox3.Text = ("Doğru dikdörtgenle çakışmıyor.");
+                }
+            }
+            else if (ydogru == y2dogru)
+            {
+                if ((xdogru > dikx1 && xdogru < dikx2) || (x2dogru > dikx1 && x2dogru < dikx2))
+                {
+                    textBox3.Text = ("Doğru dikdörtgenle çakışıyor.");
+                }
+                else
+                {
+                    textBox3.Text = ("Doğru dikdörtgenle çakışmıyor.");
+                }
+            }
+            else
+            {
+                textBox3.Text = ("Doğru eğimli olduğu için dikdörtgenle çakışması kontrol edilemiyor.");
+            }
+
+
+
+            //-------------------------------------------------------------------------------//
+
+
+            if ((noktax - xCember) * (noktax - xCember) + (noktay - yCember) * (noktay - yCember) <= x2Cember * y2Cember)
+            {
+                textBox2.Text = ("Çember noktayla çakışıyor.");
+            }
+            else
+            {
+                textBox2.Text=("Çember noktayla çakışmıyor.");
+            }
+
+
+
+
+            //-------------------------------------------------------------------------------//
+
+
+
+
+
+            // Cember ve ucgen cakısma.
+            if (((xCember - ucgenX1) * (ucgenY2 - ucgenY1) - (ucgenX2 - ucgenX1) * (yCember - ucgenY1)) * ((xCember - ucgenX2) *
+                (ucgenZ2 - yCember) - (ucgenZ1 - ucgenX2) * (yCember - yCember)) >= 0 && ((xCember - ucgenX2) * (ucgenZ2 - yCember) - (ucgenZ1 - ucgenX2) * 
+                (yCember - yCember)) * ((xCember - ucgenZ1) * (ucgenY1 - ucgenZ2) - (ucgenX1 - ucgenZ1) * (yCember - ucgenZ2)) >= 0 && ((xCember - ucgenZ1)
+                * (ucgenY1 - ucgenZ2) - (ucgenX1 - ucgenZ1) * (yCember - ucgenZ2)) * ((xCember - ucgenX1) * (yCember - ucgenY1) - (ucgenX2 - ucgenX1) * 
+                (yCember - ucgenY1)) >= 0)
+            {
+                textBox1.Text = "Çember üçgenle çakışıyor.";
+            }
+            else if ((xCember - ucgenX1) * (xCember - ucgenX1) + (yCember - ucgenY1) * (yCember - ucgenY1) <= y2Cember *
+                x2Cember || (xCember - ucgenX2) * (xCember - ucgenX2) + (yCember - yCember) * (yCember - yCember) <= y2Cember *
+                x2Cember || (xCember - ucgenZ1) * (xCember - ucgenZ1) + (yCember - ucgenZ2) * (yCember - ucgenZ2) <= y2Cember * x2Cember)
+            {
+                textBox1.Text = "Çember üçgenle çakışıyor.";
+            }
+            else
+            {
+               textBox1.Text = "Çember üçgenle çakışmıyor.";
+            }
+
+
+
+
+            //-------------------------------------------------------------------------------//
+
+
+
+            // Dikdortgen ve ucgen çakışma.
+            if ((ucgenX1 >= dikx1 && ucgenX1 <= dikx2 && ucgenY1 >= diky1 && ucgenY1 <= diky2) ||
+                (ucgenX2 >= dikx1 && ucgenX2 <= dikx2 && ucgenY2 >= diky1 && ucgenY2 <= diky2) ||
+                (ucgenZ1 >= dikx1 && ucgenZ1 <= dikx2 && ucgenZ2 >= diky1 && ucgenZ2 <= diky2))
+            {
+                textBox4.Text = ("Dikdörtgen ve üçgen çakışıyor.");
+            }
+            else
+            {
+                textBox4.Text = ("Dikdörtgen ve üçgen çakışmıyor.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string cisim1, cisim2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cisim1 = comboBox1.Text;
+            cisim2 = comboBox2.Text;
+            textBox8.Text = "      " + cisim1 + " veya  " + cisim2;
+        }
+        
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            cisim1 = comboBox1.Text;
+            cisim2 = comboBox2.Text;
+            textBox8.Text = "      " + cisim1 + " veya  " + cisim2;
+        }
+    }
+}
